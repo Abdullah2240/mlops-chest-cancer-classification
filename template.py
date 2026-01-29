15d4e94a084b5cc7724d3d9a84fdcc8496f1d988
Abdullah2240 <sheikhabdullah2240@gmail.com>
2026-01-29 16:32:33 +0500

    Requirements dowloaded & logger, exception, utils functionalities added

--- template.py
+++ template.py
@@ -9,8 +9,8 @@ project_name = "chestCancerClassifier"
 list_of_files = [
     ".github/workflows/.gitkeep",
     f"src/{project_name}/__init__.py",
-    f"src/{project_name}/components.py",
-    f"src/{project_name}/utils.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
     f"src/{project_name}/config/__init__.py",
     f"src/{project_name}/config/configuration.py",
     f"src/{project_name}/entity/__init__.py",
